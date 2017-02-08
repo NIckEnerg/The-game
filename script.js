3899f07a8f0872cd351ddd07d1f6156e2220bb96
NIckEnerg <nick-energ@yandex.ru>
2017-02-08 15:28:34 +0200

    Add vector class
    Vector class and some fixes was added. begin to construct level parser
    (level builder)

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-addEventListener('keydown', function(value) {
-	console.log(value.keyCode);
-});
+class Vector{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+
+	add(vector){
+		if (vector instanceof Vector)
+			return new Vector(vector.x + this.x, vector.u + this.y);
+		else return new Error("Not a vector");
+	}
+
+	vectorLenght(){
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+
+	multiply(k){
+		return new Vector(this.x * k, this.y *k);
+	}
+
+	addAngLength(angle, length){
+		return new Vector(this.x + Math.cos(angle) * length, this.y + Math.cos(angle) * length);
+	}
+}
+
+class Level{
+	constructor(plan){
+		this.width = plan[0].length;
+		this.height = plan.length;
+		this.grid = [];
+		this.actors = [];
+	}
+
+
+
+}
