4bb738b12de3610559b10363af6020b17e9631ce
NIckEnerg <nick-energ@yandex.ru>
2017-02-08 21:16:53 +0200

    Add some  classes of basic objects
    Such ass classes for coins, lava, player.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//standart vector class
 class Vector{
 	constructor(x, y){
 		this.x = x;
@@ -19,9 +20,45 @@ class Vector{
 	}
 
 	addAngLength(angle, length){
-		return new Vector(this.x + Math.cos(angle) * length, this.y + Math.cos(angle) * length);
+		return new Vector(this.x + Math.cos(angle) * length, this.y + Math.cos(angle) * length); // angle in PI
 	}
 }
+
+// don't think using one parent class for three children will save me any time. Player, Coin and Lava class to operate this objects.
+class Player {
+	constructor(pos){
+		this.pos = pos.add(new Vector (0, 0.5)); //because player top size x1.5 of other elements top size and we need to move it  higher
+		this.size = new Vector (0.8, 1.5); // player size
+		this.speed = new Vector (0, 0); // start player speed
+	}
+}
+Player.prototype.type = "player"; // no const now available inside class construction :/ 
+
+class Lava {
+	constructor(pos, type){
+		this.pos = pos;
+		this.size = new Vector(1, 1);
+		if (type == "="){					//different types of lava
+			this.speed = new Vector(2, 0);
+		} else if (type == "|") {
+			this.speed = new Vector(0, 2);
+		} else if (type == "v") {
+			this.speed = new Vector(0, 3);
+			this.repeatPos = pos;			// dat lava type will return to start position when touch wall
+		}
+	}
+}
+Lava.prototype.type = "lava";// sadness...
+
+//Coin for mmm... Coins! Everybody loves it. (some day replace them by dollars)
+class Coin {
+	constructor (pos){
+		this.basePos = this.pos = pos.add(new Vector(0.2, 0.1)); // coins shoud fly
+		this.size = new Vector (0.6, 0.6);
+		this.randomMove = Math.random() * Math.PI * 2; //random pos to desynchronize all coins movement
+	}
+}
+Coin.prototype.type = "coin"; 
 
 class Level{
 	//input data should be correct no data check functions here
@@ -58,5 +95,8 @@ class Level{
 Level.prototype.actorChars = {
 	"@": Player,
 	"o": Coin,
-	"!": Lava, 
+	"=": Lava, 
+	"|": Lava,
+	"v": Lava
 };
+
